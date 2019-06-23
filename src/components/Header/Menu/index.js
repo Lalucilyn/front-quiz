8692c847ee8acde55a35b2dc7a93ace7ea17b468
Lucía Wainfeld <luwainfeld@gmail.com>
2019-06-23 18:10:53 -0300

    broke header into smaller component (main, searchbar and menu)

---
+++ src/components/Header/Menu/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+const menu = (props) => {
+  const categories = [
+  	{
+  	  name: 'hombre',
+  	  subcategories: ['remeras', 'jeans']
+  	},
+  	{
+  	  name: 'mujer',
+  	  subcategories: ['remeras', 'jeans']
+  	},
+  	{
+  	  name: 'contacto'
+  	}
+  ]
+    
+  return (
+      <ul className="menu-items">
+        {categories.map(category => 
+          <li
+          onMouseEnter={() => props.toggleCategoryOpen(category.name)} 
+          onMouseLeave={() => props.toggleCategoryOpen(category.name)}>
+          <a href={`/${category.name}`}>{category.name.toUpperCase()}</a>
+            {(props.openCategory === category.name && category.subcategories) && 
+              <ul className="dropdown">
+                {category.subcategories.map(subcategory => 
+                <li>
+                  <a href={`/${category.name}/${subcategory}`}>{subcategory.toUpperCase()}</a>
+                </li>)}
+              </ul>
+            }       
+          </li>
+        )}
+      </ul>
+
+    );
+  }
+
+export default menu
---
+++ src/components/Header/Searchbar/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.scss';
+
+const searchbar = (props) => {
+  return (<input id="searchbar" type="search"/>)
+}
+export default searchbar
