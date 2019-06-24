62f464f48d6ce561fccedd7af0d7b5b945599ad4
Lucía Wainfeld <luwainfeld@gmail.com>
2019-06-23 22:58:54 -0300

    header index: fixed indentation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,53 +1,54 @@
 import React, { Component } from 'react';
-import MenuIcon from '../../static/menu_icon.png'
 import Logo from '../../static/levis_logo.png'
-import Searchbar from './Searchbar'
-import './style.scss';
 import Menu from './Menu'
+import Searchbar from './Searchbar'
+import MenuIcon from '../../static/menu_icon.png'
+import './style.scss';
+
 class Header extends Component {
-  state = {
-  	openMenu: false, 
-  	openCategory: null,
-  	mobile: false
-  }
+ state = {
+  openMenu: false, 
+  openCategory: null,
+  mobile: false
+ }
+	
+	componentDidMount() {
+  this.detectIfWeAreOnMobile()
+  window.addEventListener('resize', this.detectIfWeAreOnMobile.bind(this));
+	}
 
-  componentDidMount() {
-  	this.detectIfWeAreOnMobile()
-  	window.addEventListener('resize', this.detectIfWeAreOnMobile.bind(this));
-		}
+	componentWillUnmount() {
+  window.removeEventListener('resize', this.detectIfWeAreOnMobile.bind(this));
+	} 
 
-		componentWillUnmount() {
-  	window.removeEventListener('resize', this.detectIfWeAreOnMobile.bind(this));
-		} 
+ detectIfWeAreOnMobile() {
+  const isItAlreadyOpenedAndOnMobile = this.state.mobile && this.state.openMenu
+  const isItOnMobileNow = window.innerWidth < 640
+  this.setState({mobile: isItOnMobileNow, openMenu: isItOnMobileNow ? isItAlreadyOpenedAndOnMobile : true})
+ }
 
-  detectIfWeAreOnMobile() {
-  	const isItAlreadyOpenedAndOnMobile = this.state.mobile && this.state.openMenu
-  	const isItOnMobileNow = window.innerWidth < 640
-  	this.setState({mobile: isItOnMobileNow, openMenu: isItOnMobileNow ? isItAlreadyOpenedAndOnMobile : true})
- 	}
+ toggleCategoryOpen = (category) => {
+  let isTheCategoryAlreadyOpen = this.state.openCategory === category
+  this.setState({openCategory: isTheCategoryAlreadyOpen ? null : category})
+ }
 
-  toggleDropdownMenuOpen = () => {
-  	this.setState({openMenu: !this.state.openMenu, openCategory: null})
-  }
+ toggleDropdownMenuOpen = () => {
+  this.setState({openMenu: !this.state.openMenu, openCategory: null})
+ }
 
-  toggleCategoryOpen = (category) => {
-  	let isTheCategoryAlreadyOpen = this.state.openCategory === category
-  	this.setState({openCategory: isTheCategoryAlreadyOpen ? null : category})
-  }
-
-	 render() {
+	render() {
   return (
- 	<header className="main-header">
+ 		<header className="main-header">
  			<img id="logo" src={Logo} alt="Logo de Levi's"/>
  			{this.state.mobile && <button onClick={this.toggleDropdownMenuOpen}><img src={MenuIcon} alt="Menú desplegable"/></button>}
  			{this.state.openMenu && 
- 				<div className="header-items">
- 					<Menu toggleCategoryOpen={this.toggleCategoryOpen} openCategory={this.state.openCategory}/>
- 					<Searchbar />
- 				</div>}
- 	</header>
-
-    );
-  }
+ 			<div className="header-items">
+ 				<Menu toggleCategoryOpen={this.toggleCategoryOpen} openCategory={this.state.openCategory}/>
+ 				<Searchbar />
+ 			</div>}
+ 		</header>
+ 	);
+ }
 }
+
 export default Header
